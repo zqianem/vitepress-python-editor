8391e06047773b754e1b400cdffab18f09313f34
Em Zhan <zqianem@gmail.com>
2024-04-11 16:27:07 -0400

    Clean up template and define editor interface

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Editor from './components/Editor.vue'
+import { ref } from 'vue'
+import { type PyodideInterface, loadPyodide } from 'pyodide'
+
+let pyodide = ref<PyodideInterface | null>(null);
+(async () => pyodide.value = await loadPyodide())()
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <Editor v-if="pyodide" :pyodide />
+  <template v-else>Loading...</template>
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import type { PyodideInterface } from 'pyodide'
+import { onMounted } from 'vue'
+
+const props = defineProps<{ pyodide: PyodideInterface }>()
+
+onMounted(() => {
+  props.pyodide.runPythonAsync("1+1").then((result) => {
+    console.log("Python says that 1+1 =", result);
+  });
+})
+</script>
+
+<template></template>
