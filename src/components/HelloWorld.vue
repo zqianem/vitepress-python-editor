cd114ad9cbed80b7069bc5e84ebb29d8d32cf185
Em Zhan <zqianem@gmail.com>
2024-04-11 16:08:56 -0400

    Install pyodide

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,14 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { loadPyodide } from "pyodide";
 
-defineProps<{ msg: string }>()
+async function hello_python() {
+  let pyodide = await loadPyodide();
+  return pyodide.runPythonAsync("1+1");
+}
 
-const count = ref(0)
+hello_python().then((result) => {
+  console.log("Python says that 1+1 =", result);
+});
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
-</template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<template></template>
