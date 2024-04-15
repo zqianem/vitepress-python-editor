fb8989062b0f898f896b0a98892388841f53360a
Em Zhan <zqianem@gmail.com>
2024-04-15 09:22:58 -0500

    Make component SSR friendly
    I think? Needs testing
    
    Intentionally abusing TypeScript a little by having uninitialized
    variables with non-nullish types to avoid dealing with trying to
    manually narrow the type in callbacks

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,66 +1,70 @@
 <script lang="ts">
+let worker: Worker
+let inputData: Uint8Array
+let waitFlag: Int32Array
+
 const ready = ref(false)
-const inputBuffer = new SharedArrayBuffer(1024)
-const inputData = new Uint8Array(inputBuffer)
-const waitBuffer = new SharedArrayBuffer(4)
-const waitFlag = new Int32Array(waitBuffer)
 const encoder = new TextEncoder()
-
-const worker = new Worker(new URL('./worker.js', import.meta.url), {
-  type: 'module'
-})
-
-worker.addEventListener('message', () => {
-  ready.value = true
-  worker.postMessage({ inputBuffer, waitBuffer })
-}, { once: true })
 </script>
 
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import { EditorView, basicSetup } from 'codemirror'
-import { python } from '@codemirror/lang-python'; 
+import { python } from '@codemirror/lang-python'
 
 const props = defineProps<{ code: string }>()
 
-let parent = ref<HTMLDivElement | null>(null)
-let editor: EditorView | null = null
+const output = ref('')
+const running = ref(false)
+
+let parent = ref<HTMLDivElement>()
+let editor: EditorView
+let id: string
 
 onMounted(() => {
+  // initialize one worker per session shared by all editor instances
+  if (!worker) {
+    worker = new Worker(new URL('./worker.js', import.meta.url), {
+      type: 'module'
+    })
+    const inputBuffer = new SharedArrayBuffer(1024)
+    inputData = new Uint8Array(inputBuffer)
+    const waitBuffer = new SharedArrayBuffer(4)
+    waitFlag = new Int32Array(waitBuffer)
+
+    worker.addEventListener('message', () => {
+      ready.value = true
+      worker.postMessage({ inputBuffer, waitBuffer })
+    }, { once: true })
+  }
+  worker.addEventListener('message', async (e) => {
+    if (e.data.id !== id) return
+
+    if (e.data.input) {
+      // hack to allow time for Python input()'s prompt to output first
+      await new Promise(r => setTimeout(r, 50))
+
+      const inputArry = encoder.encode(prompt() ?? '')
+      Atomics.store(inputData, 0, inputArry.length)
+      for (let i = 0; i < inputArry.length; i++)
+        Atomics.store(inputData, i + 1, inputArry[i])
+
+      Atomics.store(waitFlag, 0, 1)
+      Atomics.notify(waitFlag, 0)
+      Atomics.store(waitFlag, 0, 0)
+    }
+    if (e.data.output) output.value += e.data.output
+    if (e.data.done) running.value = false
+  })
   editor = new EditorView({
     extensions: [basicSetup, python()],
     parent: parent.value!,
     doc: props.code
   })
-})
-
-const id = crypto.randomUUID()
-const output = ref('')
-const running = ref(false)
-
-worker.addEventListener('message', async (e) => {
-  if (e.data.id !== id) return
-
-  if (e.data.input) {
-    // hack to allow time for Python input()'s prompt to output first
-    await new Promise(r => setTimeout(r, 50)) 
-
-    const inputArry = encoder.encode(prompt() ?? '')
-    Atomics.store(inputData, 0, inputArry.length)
-    for (let i = 0; i < inputArry.length; i++)
-      Atomics.store(inputData, i + 1, inputArry[i])
-
-    Atomics.store(waitFlag, 0, 1) 
-    Atomics.notify(waitFlag, 0)
-    Atomics.store(waitFlag, 0, 0)
-  }
-  if (e.data.output) output.value += e.data.output
-  if (e.data.done) running.value = false
+  id = crypto.randomUUID()
 })
 
 function run() {
-  if (!editor) return
-
   let code = editor.state.doc.toString()
   output.value = ''
   running.value = true
