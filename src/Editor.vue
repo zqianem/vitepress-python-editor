b90e08fe575921341b4af321fc7417093d798808
Em Zhan <zqianem@gmail.com>
2024-04-19 01:44:19 -0500

    Give button title text

--- src/Editor.vue
+++ src/Editor.vue
@@ -104,13 +104,17 @@ function run() {
   running.value = true
   worker.postMessage({ id: props.id, code })
 }
+
+function buttonText() {
+  return ready.value ? (running.value ? 'Running code...' : 'Run code') : 'Loading Pyodide...'
+}
 </script>
 
 <template>
   <div class="wrapper">
     <div ref="parent" />
-    <button class="run" @click="run" :disabled="running || !ready">
-      <span class="sr-only">{{ ready ? (running ? 'Running...' : 'Run') : 'Loading Pyodide...' }}</span>
+    <button class="run" @click="run" :disabled="running || !ready" :title="buttonText()">
+      <span class="sr-only">{{ buttonText() }}</span>
       <svg v-if="!ready" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><circle cx="18" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".67" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/></circle><circle cx="12" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".33" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/></circle><circle cx="6" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin="0" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0"/></circle></svg>
       <svg v-else-if="ready && !running" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M8 17.175V6.825q0-.425.3-.713t.7-.287q.125 0 .263.037t.262.113l8.15 5.175q.225.15.338.375t.112.475t-.112.475t-.338.375l-8.15 5.175q-.125.075-.262.113T9 18.175q-.4 0-.7-.288t-.3-.712"/></svg>
       <svg v-else xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z" opacity=".5"/><path fill="currentColor" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"><animateTransform attributeName="transform" dur="1s" from="0 12 12" repeatCount="indefinite" to="360 12 12" type="rotate"/></path></svg>
