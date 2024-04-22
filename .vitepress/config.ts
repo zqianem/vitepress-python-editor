6ec314b50c82e5b6ddc0e14cc623043fb32bc2e6
Em Zhan <zqianem@gmail.com>
2024-04-22 12:26:21 -0500

    Tidy up demo site config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -3,11 +3,11 @@ import { vitepressPythonEditor } from 'vitepress-python-editor/vite-plugin'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Python Editor",
-  description: "A VitePress Component",
+  title: 'VitePress Python Editor',
+  head: [['link', { rel: 'icon', href: '/vitepress-logo-mini.svg' }]],
   srcExclude: ['README.md'],
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
   },
   vite: {
     plugins: [vitepressPythonEditor()],
